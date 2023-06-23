e1fb5b6cd97a77094c96932771b9e06aaacc2157
kavitadsharma <kavitadsharma899107@gmail.com>
2023-06-23 23:35:40 +0530

    done

--- Frontend/frontend/src/App.js
+++ Frontend/frontend/src/App.js
@@ -2,6 +2,8 @@
 import './App.css';
 
 import { useState } from 'react';
+import Choice from './pages/Choice';
+import AllRoutes from './components/AllRoutes';
 function App() {
   const butt = true;
 
@@ -19,20 +21,8 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     {/* <Choice/> */}
+     <AllRoutes/>
     </div>
   );
 }
